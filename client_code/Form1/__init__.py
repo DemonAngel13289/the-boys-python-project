58b8816f204fc1b5fb49d19ce77566fd5e34cacc
javonholmes2000@gmail.com <javonholmes2000@gmail.com>
2023-10-20 19:25:05 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,3 +7,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def date_picker_1_show(self, **event_args):
+    """This method is called when the DatePicker is shown on the screen"""
+    pass
+

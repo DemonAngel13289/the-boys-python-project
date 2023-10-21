4f40d79451e6552fcd283273f64cc32bfb2ebc32
javonholmes2000@gmail.com <javonholmes2000@gmail.com>
2023-10-21 00:33:32 +0000

    Edited Home_Page

--- client_code/Home_Page/__init__.py
+++ client_code/Home_Page/__init__.py
@@ -5,5 +5,5 @@ class Home_Page(Home_PageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    alert('Welcome to the Python Project')
     # Any code you write here will run before the form opens.

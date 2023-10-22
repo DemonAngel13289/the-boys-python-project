44687a9b68f626ec4f6d5d95a4a4d7cc74499717
javonholmes <43739121+javonholmes@users.noreply.github.com>
2023-10-21 20:05:06 -0400

    Writing my algorithm and task for TTT game project

--- local_code/Demon_Code/demon.py
+++ local_code/Demon_Code/demon.py
@@ -1,1 +1,16 @@
-print("Hello World")
+# My project is going to be Tic-Tac-Toe!
+# The goal is to make a working project in the IDE / Terminal and then
+# expand upon it to the Anvil UI
+
+# Task: Make a TTT game in Python...
+
+# Algorithm:
+# 1) Ask the user for input if they want to play the game
+# 2) If they do, initialize the lists that will store the game information.
+# otherwise, tell the user bye and end the program
+# 3) The core gameplay will be as described...
+#    -User wants to make a move, they select where to input their symbol (default to x)
+#    -The Computer will select from one of the available options and pick a place to go
+#           -Random Selection of the number by the computer based on available locations
+#    -This continues until someone loses, a cat is drawn, or the user decides to quit
+

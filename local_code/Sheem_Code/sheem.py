7cd0128bc7254632fb475b32befa691e399f4cbf
Sheem <rasheemb96@gmail.com>
2023-10-28 18:48:03 -0400

    for demon fatass

--- local_code/Sheem_Code/sheem.py
+++ local_code/Sheem_Code/sheem.py
@@ -1,1 +1,9 @@
-print("Hello World")
+print("Hey fatass")
+weight = int(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    converted = weight / 0.45
+    print("Weight in Lbs: " + str(converted))
+else:
+    converted = weight * 0.45
+    print("Weight in Kgs: " + str(converted))
